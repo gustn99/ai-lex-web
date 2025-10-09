a76d7f954c40f21b0b21319d40c1479a896406cb
gustn99 <azerosong@gmail.com>
2025-10-09 16:53:28 +0900

    feat: 우클릭 메뉴 구현

---
+++ src/components/common/Chip.tsx
@@ -0,0 +1,91 @@
+import clsx from 'clsx';
+import { Chip as ChipType } from './ChipList';
+import { useEffect, useRef, useState } from 'react';
+
+// TODO: 주석 원복
+// import ChipIcon from '@/assets/chip/deselect.svg?react'
+
+export interface ChipProps {
+	chip: ChipType;
+	isActive: boolean;
+	isAll: boolean;
+	isEditable?: boolean;
+	onSelect: (chip: string) => void;
+	onDeselect: (e: React.MouseEvent, chip: string) => void;
+}
+
+export default function Chip({ chip, isActive, isAll, isEditable = false, onSelect, onDeselect }: ChipProps) {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const menuRef = useRef<HTMLDivElement | null>(null);
+
+	const handleContextMenu = (e: React.MouseEvent) => {
+		// if (!isEditable || isAll) return;
+		e.preventDefault();
+		setIsMenuOpen(true);
+	};
+
+	const closeMenu = () => setIsMenuOpen(false);
+
+	useEffect(() => {
+		const handleOutsideClick = (e: MouseEvent) => {
+			if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+				closeMenu();
+			}
+		};
+
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, []);
+
+	const { label, value, backgroundColor } = chip;
+
+	return (
+		<div className="relative">
+			<button
+				className={clsx(
+					'relative flex items-center gap-1 rounded-lg border px-2.5 py-1.5',
+					isActive
+						? `bg-label-strong ${backgroundColor} text-inverse-label`
+						: 'border-line-normal-neutral text-label-alternative',
+				)}
+				onClick={() => onSelect(label)}
+				onContextMenu={handleContextMenu}
+			>
+				{value}
+
+				{!isAll && isActive && (
+					<div
+						role="button"
+						tabIndex={0}
+						onClick={(e) => onDeselect(e, label)}
+						className="bg-accent-background-cyan h-4 w-4"
+					>
+						{/* <ChipIcon /> */}
+					</div>
+				)}
+			</button>
+
+			{isMenuOpen && (
+				<div
+					ref={menuRef}
+					className="text-body-01-normal border-line-solid-neutral absolute top-8 my-2 flex w-40 flex-col gap-1 rounded-lg border bg-white px-3 py-2 font-normal whitespace-nowrap shadow-md"
+				>
+					{[
+						{ label: 'edit', value: '폴더 이름 변경', contentColor: 'text-label-normal' },
+						{ label: 'delete', value: '폴더 삭제', contentColor: 'text-status-negative' },
+					].map(({ label, value, contentColor }) => (
+						<button
+							key={label}
+							className={clsx('border-line-normal-alternative flex gap-2 py-2 not-last:border-b', contentColor)}
+						>
+							<div className="bg-accent-background-cyan h-6 w-6" />
+							{value}
+						</button>
+					))}
+				</div>
+			)}
+		</div>
+	);
+}
--- src/components/common/ChipList.tsx
+++ src/components/common/ChipList.tsx
@@ -1,6 +1,4 @@
-import clsx from 'clsx';
-// TODO: 주석 원복
-// import ChipIcon from '@/assets/chip/deselect.svg?react'
+import Chip from './Chip';
 
 export interface Chip {
 	label: string;
@@ -41,35 +39,17 @@ export default function ChipList({ chips, activeChips, isEditable = false, setAc
 
 	return (
 		<div className="text-label-01-normal flex gap-1.5 font-medium">
-			{chips.map(({ label, value, backgroundColor }) => {
-				const isActive = activeChips.includes(label);
-				const isAll = label === 'all';
-
-				// TODO: bg 부분 twMerge 적용
-				return (
-					<button
-						className={clsx(
-							'flex items-center gap-1 rounded-lg border px-2.5 py-1.5',
-							isActive
-								? `bg-label-strong ${backgroundColor} text-inverse-label`
-								: 'border-line-normal-neutral text-label-alternative',
-						)}
-						onClick={() => handleSelect(label)}
-					>
-						{value}
-						{!isAll && isActive && (
-							<div
-								role="button"
-								tabIndex={0}
-								onClick={(e) => handleDeselect(e, label)}
-								className="bg-accent-background-cyan h-4 w-4"
-							>
-								{/* <ChipIcon /> */}
-							</div>
-						)}
-					</button>
-				);
-			})}
+			{chips.map((c) => (
+				<Chip
+					key={c.label}
+					chip={c}
+					isActive={activeChips.includes(c.label)}
+					isAll={c.label === 'all'}
+					isEditable={isEditable}
+					onSelect={handleSelect}
+					onDeselect={handleDeselect}
+				/>
+			))}
 		</div>
 	);
 }
