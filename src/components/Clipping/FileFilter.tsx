5bf02fc6175ea06ad53cedebd8f60816a38438d4
gustn99 <azerosong@gmail.com>
2025-10-09 14:59:37 +0900

    fix: isAll 로직 수정

--- src/components/Clipping/FileFilter.tsx
+++ src/components/Clipping/FileFilter.tsx
@@ -1,45 +1,52 @@
 import { useState } from 'react';
 import ChipList, { Chip } from '../common/ChipList';
 
-const chips: Chip[] = [
+const chips: (Chip & { type: 'document' | 'party' })[] = [
 	{
+		type: 'document',
 		label: 'all',
 		value: '전체',
 	},
 	{
+		type: 'document',
 		label: 'writtenStatement',
 		value: '서면',
 	},
 	{
+		type: 'document',
 		label: 'documentaryEvidence',
 		value: '서증',
 	},
 	{
+		type: 'document',
 		label: 'etc',
 		value: '그 외',
 	},
 	{
+		type: 'party',
 		label: 'plaintiff',
 		value: '원고',
 		backgroundColor: 'bg-tag-red',
 	},
 	{
+		type: 'party',
 		label: 'defendant',
 		value: '피고',
 		backgroundColor: 'bg-tag-blue',
 	},
 ];
 
-const defaultChip = chips[0].label;
-
 export default function FileFilter() {
-	const [activeChips, setActiveChips] = useState([defaultChip]);
+	const documentChips = chips.filter((c) => c.type === 'document');
+	const partyChips = chips.filter((c) => c.type === 'party');
+
+	const [activeChips, setActiveChips] = useState(['all']);
 
 	const handleSelect = (chip: string) => {
-		if (chip === defaultChip) {
-			setActiveChips([defaultChip]);
+		if (chip === 'all') {
+			setActiveChips(['all']);
 		} else {
-			const filteredChips = activeChips.filter((c) => c !== defaultChip);
+			const filteredChips = activeChips.filter((c) => c !== 'all');
 			filteredChips.push(chip);
 			setActiveChips(filteredChips);
 		}
@@ -47,18 +54,22 @@ export default function FileFilter() {
 
 	const handleDeselect = (e: React.MouseEvent, chip: string) => {
 		e.stopPropagation();
-		if (chip === defaultChip) return;
+		if (chip === 'all') return;
 
 		const filteredChips = activeChips.filter((c) => c !== chip);
 		if (filteredChips.length === 0) {
-			filteredChips.push(defaultChip);
+			filteredChips.push('all');
 		}
 		setActiveChips(filteredChips);
 	};
 
 	return (
 		<div className="p-4">
-			<ChipList chips={chips} activeChips={activeChips} onSelect={handleSelect} onDeselect={handleDeselect} />
+			<div className="flex items-center gap-1.5">
+				<ChipList chips={documentChips} activeChips={activeChips} onSelect={handleSelect} onDeselect={handleDeselect} />
+				<div className="bg-line-normal-normal h-4 w-px" />
+				<ChipList chips={partyChips} activeChips={activeChips} onSelect={handleSelect} onDeselect={handleDeselect} />
+			</div>
 		</div>
 	);
 }
--- src/components/common/ChipList.tsx
+++ src/components/common/ChipList.tsx
@@ -18,14 +18,14 @@ export interface ChipListProps {
 export default function ChipList({ chips, activeChips, isEditable = false, onSelect, onDeselect }: ChipListProps) {
 	return (
 		<div className="text-label-01-normal flex gap-1.5 font-medium">
-			{chips.map(({ label, value, backgroundColor }, i) => {
+			{chips.map(({ label, value, backgroundColor }) => {
 				const isActive = activeChips.includes(label);
-				const isAll = i === 0;
+				const isAll = label === 'all';
 
 				return (
 					<button
 						className={clsx(
-							'flex items-center gap-1 rounded-lg px-2.5 py-1.5',
+							'flex items-center gap-1 rounded-lg border px-2.5 py-1.5',
 							isActive
 								? `bg-label-strong ${backgroundColor} text-inverse-label`
 								: 'border-line-normal-neutral text-label-alternative',
