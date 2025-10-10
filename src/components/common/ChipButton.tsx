58be933f63c14eb4f7d68c4ee8f746244aee71c9
gustn99 <azerosong@gmail.com>
2025-10-10 17:32:00 +0900

    feat: svgr 및 tw merge 적용

--- src/components/common/ChipButton.tsx
+++ src/components/common/ChipButton.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
-import { Chip } from './ChipList';
+import { twMerge } from 'tailwind-merge';
 
-// TODO: 주석 원복
-// import ChipIcon from '@/assets/chip/deselect.svg?react'
+import ChipIcon from '@/assets/chip/deselect.svg?react';
+
+import { Chip } from './ChipList';
 
 export interface ChipButtonProps {
 	chip: Chip;
@@ -24,21 +25,22 @@ export default function ChipButton({
 	setIsMenuOpen,
 }: ChipButtonProps) {
 	const handleContextMenu = (e: React.MouseEvent) => {
-		// if (!isEditable || isAll) return;
+		if (!isEditable || isAll) return;
 		e.preventDefault();
 		setIsMenuOpen(true);
 	};
 
 	const { label, value, backgroundColor } = chip;
 
-	// TODO: bg twMerge 적용
 	return (
 		<button
-			className={clsx(
-				'flex items-center gap-1 rounded-lg border px-2.5 py-1.5',
-				isActive
-					? `bg-label-strong ${backgroundColor} text-inverse-label`
-					: 'border-line-normal-neutral text-label-alternative',
+			className={twMerge(
+				clsx(
+					'flex items-center gap-1 rounded-lg border px-2.5 py-1.5',
+					isActive
+						? `bg-label-strong ${backgroundColor} text-inverse-label`
+						: 'border-line-normal-neutral text-label-alternative',
+				),
 			)}
 			onClick={() => onSelect(label)}
 			onContextMenu={handleContextMenu}
@@ -46,13 +48,8 @@ export default function ChipButton({
 			{value}
 
 			{!isAll && isActive && (
-				<div
-					role="button"
-					tabIndex={0}
-					onClick={(e) => onDeselect(e, label)}
-					className="bg-accent-background-cyan h-4 w-4"
-				>
-					{/* <ChipIcon /> */}
+				<div role="button" tabIndex={0} onClick={(e) => onDeselect(e, label)}>
+					<ChipIcon />
 				</div>
 			)}
 		</button>
