2abdaec7e1b83fa7bc8dbb923ab1f4bb774e9847
chaeyoungwon <epopcy@naver.com>
2025-10-12 04:44:44 +0900

    feat: 하단으로 이동 시 자연스럽게 이동하도록 transition 효과 추가

--- src/pages/ChatBot/ChatBot.tsx
+++ src/pages/ChatBot/ChatBot.tsx
@@ -21,30 +21,29 @@ export default function ChatBot() {
 	return (
 		<div className="flex h-screen flex-col">
 			<ChatHeader title={title} caseNo={caseNo} />
+
 			<div className="flex flex-1">
 				<ChatSidebar />
 
-				<main className="relative flex flex-1 flex-col bg-white">
-					{!isChatStarted ? (
-						<div className="flex flex-1 flex-col items-center justify-center transition-all duration-500">
-							<h2 className="text-label-normal text-title-03 font-bold">안녕하세요, 어떤 질문이 있으신가요?</h2>
-							<ChatCategorySection
-								selectedCategory={selectedCategory}
-								onCategoryClick={handleCategoryClick}
-								onSubmit={handleStartChat}
-								isFixed={false}
-							/>
-						</div>
-					) : (
-						<div className="absolute bottom-[10px] left-0 mt-[10px] w-full bg-white">
-							<ChatCategorySection
-								selectedCategory={selectedCategory}
-								onCategoryClick={handleCategoryClick}
-								onSubmit={handleStartChat}
-								isFixed={true}
-							/>
-						</div>
-					)}
+				<main className="relative flex flex-1 flex-col overflow-hidden bg-white">
+					<div
+						className={`absolute left-0 w-full bg-white transition-transform duration-400 ease-[cubic-bezier(0.25,1,0.5,1)] ${
+							isChatStarted ? 'bottom-[10px] translate-y-0 opacity-100' : 'top-1/2 -translate-y-1/2 opacity-100'
+						}`}
+					>
+						{!isChatStarted && (
+							<div className="flex flex-col items-center justify-center">
+								<h2 className="text-label-normal text-title-03 font-bold">안녕하세요, 어떤 질문이 있으신가요?</h2>
+							</div>
+						)}
+
+						<ChatCategorySection
+							selectedCategory={selectedCategory}
+							onCategoryClick={handleCategoryClick}
+							onSubmit={handleStartChat}
+							isFixed={isChatStarted}
+						/>
+					</div>
 				</main>
 			</div>
 		</div>
