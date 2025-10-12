972d659e7448952143872bb327178aac756c2783
gustn99 <azerosong@gmail.com>
2025-10-12 12:42:51 +0900

    refactor: file list hidden 로직 변경

--- src/components/common/Input/FileList.tsx
+++ src/components/common/Input/FileList.tsx
@@ -9,11 +9,10 @@ export default function FileList({
 		setFiles(files.filter((f) => f.name !== fileName));
 	};
 
+	if (files.length === 0) return null;
+
 	return (
-		<ul
-			hidden={files.length === 0}
-			className="border-line-normal-neutral file-uploader-scrollbar flex max-h-64 w-full flex-col gap-2 overflow-y-scroll rounded-lg border p-2 pr-0"
-		>
+		<ul className="border-line-normal-neutral file-uploader-scrollbar flex max-h-64 w-full flex-col gap-2 overflow-y-scroll rounded-lg border p-2 pr-0">
 			{files.map((f) => (
 				<li key={f.name} className="text-label-02 grid grid-cols-[auto_1fr_auto] items-center gap-1 px-1">
 					<div className="bg-accent-background-cyan h-[18px] w-[18px]"></div>
