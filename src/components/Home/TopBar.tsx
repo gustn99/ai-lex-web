d0c2a3e63fa5e7d278d80b50537691b54acadad8
gustn99 <azerosong@gmail.com>
2025-10-12 14:16:51 +0900

    feat: 탑바 및 홈 헤더 구현

---
+++ src/components/Home/TopBar.tsx
@@ -0,0 +1,27 @@
+import { useLocation } from 'react-router-dom';
+
+import PlusIcon from '@/assets/svgs/common/plus.svg?react';
+import LogoIcon from '@/assets/svgs/home/logo.svg?react';
+
+import { Button } from '../common/Button';
+
+export default function TopBar() {
+	const location = useLocation();
+	const isTrueHome = location.pathname === '/';
+
+	return (
+		<div className="border-line-normal-neutral sticky flex h-15 w-full items-center border bg-white/10 px-6 md:px-15 lg:px-30">
+			<div className="mx-auto flex w-full max-w-[1100px] items-center justify-between">
+				<LogoIcon className="my-1.5" />
+				<div className="flex items-center gap-4">
+					<p>user1234</p>
+					{!isTrueHome && (
+						<Button appearance="solid" size="medium" LeadingIcon={PlusIcon}>
+							서면 생성하기
+						</Button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,19 @@
 import BackgroundImage from '@/components/Home/BackgroundImage';
 import FileUploaderContent from '@/components/Home/FileUploaderContent';
+import TopBar from '@/components/Home/TopBar';
 
 export default function Home() {
 	return (
-		<div>
-			<FileUploaderContent />
-			<BackgroundImage />
-		</div>
+		<>
+			<TopBar />
+			<div className="mt-11 flex flex-col items-center gap-11">
+				<div className="space-y-3 text-center">
+					<p className="text-title-01 font-bold">사건 서류를 등록해 주세요.</p>
+					<p className="text-heading-01">Pdf 형식의 파일을 업로드 해주세요.</p>
+				</div>
+				<FileUploaderContent />
+				<BackgroundImage />
+			</div>
+		</>
 	);
 }
