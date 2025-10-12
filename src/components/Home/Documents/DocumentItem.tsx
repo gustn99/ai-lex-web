b9533c45101de8800f6c0b370a3af50861128d92
gustn99 <azerosong@gmail.com>
2025-10-12 22:37:07 +0900

    feat: document list 컴포넌트 구현

--- src/components/Home/Documents/DocumentItem.tsx
+++ src/components/Home/Documents/DocumentItem.tsx
@@ -19,22 +19,23 @@ export default function DocumentItem({
 	createdAt,
 	updatedAt,
 }: DocumentItemProps) {
-	const partyContent = party === 'plaiintiff' ? '피고측 입장' : '원고측 입장';
+	const isPlaintiff = party === 'plaintiff';
+	const partyContent = isPlaintiff ? '원고측 입장' : '피고측 입장';
 	const dateContent = updatedAt ? `${updatedAt} 업데이트` : `${createdAt} 생성`;
 
 	return (
 		<a href={href} className="border-line-normal-neutral flex flex-col rounded-xl border p-3">
 			<div className="mb-2 flex justify-between">
-				<ContentBadge appearance="solid" color="blue" className="my-0.5">
+				<ContentBadge appearance="solid" color={isPlaintiff ? 'red' : 'blue'} className="my-0.5">
 					{partyContent}
 				</ContentBadge>
 				{description && <p className="text-label-02 text-primary-normal font-semibold">{description}</p>}
 			</div>
 
 			<div className="mb-4 flex items-center gap-1">
-				<span className="text-body-01-normal font-semibold">{title}</span>
+				<span className="text-body-01-normal truncate font-semibold">{title}</span>
 				<div className="bg-line-normal-normal h-3 w-px" />
-				<span className="text-label-01-normal text-label-neutral">{caseNumber}</span>
+				<span className="text-label-01-normal text-label-neutral truncate">{caseNumber}</span>
 			</div>
 
 			<div className="text-label-02 text-label-alternative">{dateContent}</div>
--- src/pages/Home/Documents/Documents.tsx
+++ src/pages/Home/Documents/Documents.tsx
@@ -1,12 +1,17 @@
 import Banner from '@/components/Home/Documents/Banner';
+import DocumentList from '@/components/Home/Documents/DocumentList';
 import TopBar from '@/components/Home/TopBar';
 
 export default function Documents() {
 	return (
 		<>
 			<TopBar />
-			<div className="mx-auto mt-6 flex max-w-[1100px] flex-col items-center gap-6 px-4">
+			<div className="mx-auto mt-6 flex max-w-[1100px] flex-col items-center gap-6 px-4 pb-40">
 				<Banner />
+				<div className="w-full space-y-10">
+					<DocumentList title="추천 작업" />
+					<DocumentList title="사건" />
+				</div>
 			</div>
 		</>
 	);
