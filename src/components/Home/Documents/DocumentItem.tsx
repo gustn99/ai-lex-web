087a3583a78fda180b59a7e323866cdc4e19f94c
gustn99 <azerosong@gmail.com>
2025-10-12 15:31:26 +0900

    feat: document item 컴포넌트 구현

---
+++ src/components/Home/Documents/DocumentItem.tsx
@@ -0,0 +1,43 @@
+import ContentBadge from '@/components/common/ContentBadge';
+
+interface DocumentItemProps {
+	href: string;
+	party: string;
+	description?: string;
+	title: string;
+	caseNumber: string;
+	createdAt?: string;
+	updatedAt?: string;
+}
+
+export default function DocumentItem({
+	href,
+	party,
+	description,
+	title,
+	caseNumber,
+	createdAt,
+	updatedAt,
+}: DocumentItemProps) {
+	const partyContent = party === 'plaiintiff' ? '피고측 입장' : '원고측 입장';
+	const dateContent = updatedAt ? `${updatedAt} 업데이트` : `${createdAt} 생성`;
+
+	return (
+		<a href={href} className="border-line-normal-neutral flex flex-col rounded-xl border p-3">
+			<div className="mb-2 flex justify-between">
+				<ContentBadge appearance="solid" color="blue" className="my-0.5">
+					{partyContent}
+				</ContentBadge>
+				{description && <p className="text-label-02 text-primary-normal font-semibold">{description}</p>}
+			</div>
+
+			<div className="mb-4 flex items-center gap-1">
+				<span className="text-body-01-normal font-semibold">{title}</span>
+				<div className="bg-line-normal-normal h-3 w-px" />
+				<span className="text-label-01-normal text-label-neutral">{caseNumber}</span>
+			</div>
+
+			<div className="text-label-02 text-label-alternative">{dateContent}</div>
+		</a>
+	);
+}
