3f076573f5063131e48cb910e573874a1c555942
gustn99 <azerosong@gmail.com>
2025-10-13 13:16:55 +0900

    feat: 디테일 페이지 라우트 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import DocumentDetail from '@/pages/Documents/DocumentDetail';
 import Home from '@/pages/Home/Home';
 
-export const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+export const router = createBrowserRouter([
+	{ path: '/', element: <Home /> },
+	{
+		path: '/documents',
+		children: [
+			// {
+			// 	index: true, // /documents
+			// 	element: <DocumentList />,
+			// },
+			{
+				path: ':id', // /documents/:id
+				element: <DocumentDetail />,
+			},
+		],
+	},
+]);
