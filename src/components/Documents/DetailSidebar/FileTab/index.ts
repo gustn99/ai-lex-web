728787c06067d25633a724212f47ff769bb32e35
gustn99 <azerosong@gmail.com>
2025-10-13 18:22:42 +0900

    refactor: 탭별 폴더 분리

---
+++ src/components/Documents/DetailSidebar/FileTab/index.ts
@@ -0,0 +1,1 @@
+export { default as FileFilter } from './FileFilter';
--- src/components/Documents/DetailSidebar/index.ts
+++ src/components/Documents/DetailSidebar/index.ts
@@ -1,3 +1,2 @@
 export { default as DetailSidebar } from './DetailSidebar';
 export { default as DetailSidebarTabs } from './DetailSidebarTabs';
-export { default as FileFilter } from './FileFilter';
